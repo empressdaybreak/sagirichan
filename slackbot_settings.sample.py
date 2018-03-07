65fd1ab6fc0c6189ed4153b09710f3d358689563
DayBreak <as616111@gmail.com>
2018-03-08 00:12:56 +0900

    기본 설정 파일 추가

---
+++ slackbot_settings.sample.py
@@ -0,0 +1,10 @@
+API_TOKEN = ""
+
+DEFAULT_REPLY = "죄송하지만 무슨 말인지 모르겠어요.."
+
+ERRORS_TO = 'chatbot-dev'
+
+PLUGINS = ['slackbot.plugins', 'sagiribot',]
+~                                                                  
+~                                                                  
+~                                               
